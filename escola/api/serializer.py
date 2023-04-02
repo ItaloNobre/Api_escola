119e6f7dd239a92bf2140d0f9766357060e6af5a
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-04-01 21:15:31 -0300

    adicionando validações para Aluno

--- escola/api/serializer.py
+++ escola/api/serializer.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 from escola.models import Aluno, Curso, Matricula
+from escola.validators import *
 
 
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
         fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': "o Cpf não é valido"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': "nao inclua números neste campo"})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': " o RG deve conter 9 digitos"})
+        return data
 
 
 class CursoSerializer(serializers.ModelSerializer):
